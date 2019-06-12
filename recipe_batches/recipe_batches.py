6b2c96eeb96c7e7992ab473a39f480ef8a9aa088
Rob Salzberg <robsalzberg@gmail.com>
2019-06-11 23:06:29 -0400

    Completed recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,25 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    # If the # of ingredients available is less than the number needed for a recipe than return 0
+    if len(recipe) > len(ingredients):
+        batches = 0
+    else:
+        result = [ingredients[k]/recipe[k] for k in recipe if k in ingredients]
+        # Divide the amount of ingredients available by the amount needed for a recipe
+        # 280/60 = 4, 51/25 =2 , 51/20 =2
+        batches = math.floor(min(result))  
+        # The number of batches is the minimum amount of times you can get the amount needed for a recipe from the available ingredients
+        # Minimum Batches = 2
+    return batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 60, 'butter': 25, 'flour': 20}
+    ingredients = {'milk': 280, 'butter': 51, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
