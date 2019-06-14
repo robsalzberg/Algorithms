89f3e91f68f585e44ab2b26832ad8fbce44e48c0
Rob Salzberg <robsalzberg@gmail.com>
2019-06-14 00:56:12 -0400

    Completed rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,28 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+  choices = ['rock', 'paper', 'scissors']
+  possible_plays = []
+
+  def find_possible_play(n, result=[]):
+# Base case
+    if n == 0:
+      possible_plays.append(result)
+      return
+    for choice in choices:
+# Helper function
+# Move towards the base case
+      find_possible_play(n-1, result + [choice])
+# Call helper function
+  find_possible_play(n, [])
+  return possible_plays
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_choices = int(sys.argv[1])
+        print(rock_paper_scissors(num_choices))
+    else:
+        print('Usage: rps.py [num_choices]')
